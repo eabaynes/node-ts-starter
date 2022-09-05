d08a9ba46b5b96b75108079db89a7db34db7fdcf
Manav Misra <manavm@visionwebsoft.com>
2022-09-05 09:51:52 -0500

    Enable test coverage output

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,9 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   test: {
+    coverage: {
+      reporter: ['text'],
+    },
     include: ['src/**/*.test.ts'],
-    passWithNoTests: true,
   },
 });
